dff3edbfb7032b493192eb552ec24633e4083ce0
Lorenz Seufert <lorenzseufert@gmail.com>
2020-06-12 17:25:04 +0200

    FigureBean Finished

---
+++ Shogi/src/FigureType.java
@@ -0,0 +1,13 @@
+public enum FigureType {
+    UNDEFINED,
+    KING,
+    TOWER,
+    BISHOP,
+    GOLDEN_GENERAL,
+    SILVER_GENERAL,
+    KNIGHT,
+    LANCE,
+    PAWN,
+    DRAGON,
+    HORSE
+}

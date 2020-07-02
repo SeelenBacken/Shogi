353f2dafa7010820f38aed465427825445866705
Krayaty <66520939+Krayaty@users.noreply.github.com>
2020-07-02 17:42:00 +0200

    Add files via upload Ordered the project-structure and added a Servlet and web.xml-file to connect single websites. Spielbrett-Zustand zentral als JSON spiechern. update update update

--- Shogi/src/beans/GameBean.java
+++ Shogi/src/beans/GameBean.java
@@ -5,14 +5,23 @@ import dataclasses.History;
 import dataclasses.Player;
 import enums.TeamType;
 
+/**
+ * The an instance of this class is a bean representing a game. The name of the
+ * players should be set and if there is a bot in this game the bot should set
+ * right after instantiation.
+ *
+ */
 public class GameBean {
 
 	private int round;
 	private final History history;
-	private final Player white;
-	private final Player black;
+	private Player white;
+	private Player black;
 	private final GameBoard board;
 
+	/**
+	 * The constructor of this class.
+	 */
 	public GameBean() {
 		round = 0;
 		history = new History(10);
@@ -21,23 +30,81 @@ public class GameBean {
 		board = new GameBoard(9);
 	}
 
+	/**
+	 * Getter of the history.
+	 * 
+	 * @return the history
+	 */
 	public History getHistory() {
 		return history;
 	}
 
+	/**
+	 * Getter of the white player.
+	 * 
+	 * @return the white player
+	 */
 	public Player getWhite() {
 		return white;
 	}
 
+	/**
+	 * Getter of the black player.
+	 * 
+	 * @return the black player
+	 */
 	public Player getBlack() {
 		return black;
 	}
+	
+	//-------------------------------------------------------------
+	public int getPlayerTeam(int x, int y) {
+		if(this.getBoard().getBoard()[x][y] != null) {
+			Player p = (this.getBlack().getName().equals("AI")) ? this.getWhite() : this.getBlack();
+			
+			int res = (this.getBoard().getBoard()[x][y].getTeam().equals(p.getTeam())) ? 1 : 2;
+			return res;
+		} 
+		return 1;
+		
+	}
+	
+	//___-------------------------------------------------------
 
+	/**
+	 * Getter of the game board.
+	 * 
+	 * @return the game board
+	 */
 	public GameBoard getBoard() {
 		return board;
 	}
+	
+	
+	/**
+	 * Setter of the round. Should only used with 1 or -1.
+	 * 
+	 * @param i the number of rounds to be incremented.
+	 */
+	public void setRound(int i) {
+		this.round += i;
+	}
 
-	public Player getCurrentPlayer() {
-		return (++round % 2 == 1) ? white : black;
+	/**
+	 * Setter of the white player, should only be used if the white player is an AI
+	 * 
+	 * @param white the ai player
+	 */
+	public void setWhite(Player white) {
+		this.white = white;
+	}
+
+	/**
+	 * Setter of the black player, should only be used if the black player is an AI
+	 * 
+	 * @param black the ai player
+	 */
+	public void setBlack(Player black) {
+		this.black = black;
 	}
 }
--- Shogi/src/dataclasses/Figure.java
+++ Shogi/src/dataclasses/Figure.java
@@ -1,26 +1,88 @@
 package dataclasses;
 
 import java.security.InvalidParameterException;
+import java.util.ArrayList;
 
 import enums.FigureType;
 import enums.TeamType;
 
+/**
+ * This class represents a figure on the game board. This class can only be
+ * instantiated using the static methods listed below, to ensure correct that
+ * every figure is valid.
+ * 
+ * @see FigureType
+ * @see TeamType
+ * 
+ * @see #Figure(FigureType, FigureType, TeamType)
+ * @see #getKing(TeamType)
+ * @see #getTower(TeamType)
+ * @see #getBishop(TeamType)
+ * @see #getGoldenGeneral(TeamType)
+ * @see #getSilverGeneral(TeamType)
+ * @see #getKnight(TeamType)
+ * @see #getLance(TeamType)
+ * @see #getPawn(TeamType)
+ * @see #getDragon(TeamType)
+ * @see #getHorse(TeamType)
+ */
 public class Figure {
+
 	private FigureType type;
 	private FigureType changeType;
 
 	private final TeamType team;
 
-	public Figure(FigureType type, FigureType changeType, TeamType team) {
+	/**
+	 * The constructor of this class, cannot be invoked directly only by using the
+	 * static getFigur methods.
+	 * 
+	 * @param type       of the figure
+	 * @param changeType if the figur can be upgraded or has to be degraded the it
+	 *                   will be changed to this using {@link #changeType()}.
+	 * @param team       of this Figure if set to {@link TeamType#NONE} the figure
+	 *                   will not be considered as valid.
+	 * 
+	 * @throws InvalidParameterException if the team is set as {@link TeamType#NONE}
+	 * 
+	 * @see #getKing(TeamType)
+	 * @see #getTower(TeamType)
+	 * @see #getBishop(TeamType)
+	 * @see #getGoldenGeneral(TeamType)
+	 * @see #getSilverGeneral(TeamType)
+	 * @see #getKnight(TeamType)
+	 * @see #getLance(TeamType)
+	 * @see #getPawn(TeamType)
+	 * @see #getDragon(TeamType)
+	 * @see #getHorse(TeamType)
+	 */
+	private Figure(FigureType type, FigureType changeType, TeamType team) {
+		if (team == TeamType.NONE) {
+			throw new InvalidParameterException("Invalid team for a figur: " + TeamType.NONE);
+		}
 		this.type = type;
 		this.changeType = changeType;
 		this.team = team;
 	}
 
+	/**
+	 * Getter of the type.
+	 * 
+	 * @return the type
+	 * 
+	 * @see #Figure(FigureType, FigureType, TeamType)
+	 */
 	public FigureType getType() {
 		return type;
 	}
 
+	/**
+	 * Getter of the changeType.
+	 * 
+	 * @return the changeType
+	 * 
+	 * @see #Figure(FigureType, FigureType, TeamType)
+	 */
 	public FigureType getChangeType() {
 		return changeType;
 	}
@@ -29,6 +91,10 @@ public class Figure {
 		return team;
 	}
 
+	/**
+	 * Considers an instance of this class equal to any instance of this class for
+	 * which the type, the changeType and the team are equal.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (obj instanceof Figure) {
@@ -38,6 +104,15 @@ public class Figure {
 		return false;
 	}
 
+	/**
+	 * Changes the type of this figure by switching the type with the changeType if
+	 * {@link #canChangeType()} returns true, more formally changes the type of this
+	 * figure if and only if <b>{@code changeType != FigureType.UNDEFINED}</b> by
+	 * swapping them.
+	 * 
+	 * @see FigureType
+	 * @see #canChangeType()
+	 */
 	public void changeType() {
 		if (canChangeType()) {
 			FigureType temp = type;
@@ -46,9 +121,152 @@ public class Figure {
 		}
 	}
 
+	/**
+	 * Evaluates if this figure can move from the currPos to the targetPos on the
+	 * board by calling
+	 * {@link #canMoveThere(Vector2, Vector2, GameBoard, ArrayList)} with the last
+	 * parameter set to {@code null}.
+	 * 
+	 * @param currPos   the current position of this figure on the board.
+	 * @param targetPos the target position or more formally the absolute position
+	 *                  on the board where this figure shall move at.
+	 * @param board     the board this figure stands on.
+	 * 
+	 * @return {@code true} if and only if the path is not obstructed and the
+	 *         targetPos is empty or at least occupied by an enemy, otherwise
+	 *         {@code false}.
+	 * 
+	 * @throws InvalidParameterException if currPos, targetPos or board is null.
+	 * 
+	 * @see #canMoveThere(Vector2, Vector2, GameBoard, ArrayList)
+	 */
+	public boolean canMoveThere(Vector2 currPos, Vector2 targetPos, GameBoard board) {
+		return canMoveThere(currPos, targetPos, board, null);
+	}
+
+	/**
+	 * Evaluates if this figure can move from the currPos to and on the target
+	 * position. If path is not set to {@code null} all absolute Positions the path
+	 * is made of will be added to this.
+	 * 
+	 * @param currPos   the current position of this figure on the board.
+	 * @param targetPos the target position or more formally the absolute position
+	 *                  on the board where this figure shall move at.
+	 * @param board     the board this figure stands on.
+	 * @param path      the path this figure will have to walk to the targetPos (not
+	 *                  including this, due to this should always be known) will be
+	 *                  added to this if not set to {@code null}.
+	 * 
+	 * @return {@code true} if and only if the path is not obstructed and the
+	 *         targetPos is empty or at least occupied by an enemy, otherwise
+	 *         {@code false}.
+	 * 
+	 * @throws InvalidParameterException if currPos, targetPos or board is null.
+	 */
+	public boolean canMoveThere(Vector2 currPos, Vector2 targetPos, GameBoard board, ArrayList<Vector2> path) {
+		if (currPos == null) {
+			throw new InvalidParameterException("currPos cannot be null!");
+		}
+		if (targetPos == null) {
+			throw new InvalidParameterException("targetPos cannot be null!");
+		}
+		if (board == null) {
+			throw new InvalidParameterException("board cannot be null!");
+		}
+		Vector2 move = Vector2.sub(targetPos, currPos);
+		if (isValidMove(move) == false) {
+			return false;
+		}
+		boolean calculatePath = path != null;
+		move.shortenIfPossible();
+		Vector2 currPosCopy = Vector2.add(currPos, move);
+		for (; !currPosCopy.equals(targetPos); currPosCopy = Vector2.add(currPosCopy, move)) {
+			if (board.ensureTeamTypeForPosition(currPosCopy) == TeamType.NONE) {
+				if (calculatePath) {
+					path.add(new Vector2(currPosCopy));
+				}
+				continue;
+			}
+			return false;
+		}
+		return team != board.ensureTeamTypeForPosition(currPosCopy);
+	}
+
+	/**
+	 * Gets the relative file path of the picture. depending on type and changeType,
+	 * as a String
+	 * 
+	 * @return the relative file path of the picture as a String
+	 */
+	public String getPicturePath() {		
+		if (team == TeamType.NONE) {
+			throw new IllegalStateException("Team of this figure is " + TeamType.NONE + "!");
+		}
+		String path = "../res/half/";
+		outer: switch (type) {
+		case KING:
+			path += ((team == TeamType.WHITE) ? "king" : "jewel");
+			break;
+		case TOWER:
+			path += "rook";
+			break;
+		case BISHOP:
+			path += "bishop";
+			break;
+		case GOLDEN_GENERAL:
+			switch (changeType) {
+			case SILVER_GENERAL:
+				path += "prosilver";
+				break outer;
+			case KNIGHT:
+				path += "proknight";
+				break outer;
+			case LANCE:
+				path += "prolance";
+				break outer;
+			case PAWN:
+				path += "tokin";
+				break outer;
+			default:
+				path += "gold";
+				break outer;
+			}
+		case SILVER_GENERAL:
+			path += "silver";
+			break;
+		case KNIGHT:
+			path += "knight";
+			break;
+		case LANCE:
+			path += "lance";
+			break;
+		case PAWN:
+			path += "pawn";
+			break;
+		case DRAGON:
+			path += "dragon";
+			break;
+		case HORSE:
+			path += "horse";
+			break;
+		default:
+			throw new IllegalStateException("Figure type is " + FigureType.UNDEFINED + "!");
+		}
+		return path += ".png";
+	}
+
+	/**
+	 * Evaluates if a move is valid more formally if the relative {@link Vector2}
+	 * move is valid for this figure's type as defined in the game rules.
+	 * 
+	 * @param move a relative {@link Vector2} representing the move.
+	 * 
+	 * @return {@code true} if and only if the move is valid, otherwise
+	 *         {@code false}.
+	 */
 	public boolean isValidMove(Vector2 move) {
 		if (move == null) {
-			throw new NullPointerException("Move is not allowed to be null!");
+			throw new InvalidParameterException("Move is not allowed to be null!");
 		}
 		if (team == TeamType.NONE) {
 			throw new InvalidParameterException("Move cannot be valid if the figure does not belong to a Team!");
@@ -86,7 +304,176 @@ public class Figure {
 		}
 	}
 
+	/**
+	 * Evaluates if this figure can be upgraded or degraded, more formally if
+	 * {@code changeType != FigureType.UNDEFINED}.
+	 * 
+	 * @return {@code true} if and only if
+	 *         {@code changeType != FigureType.UNDEFINED}, otherwise {@code false}.
+	 */
 	private boolean canChangeType() {
 		return changeType != FigureType.UNDEFINED;
+	}
+
+	/**
+	 * Gets a valid instance of this class representing a king according to the game
+	 * rules belonging to the team. Has to be used instead of Constructor.
+	 * 
+	 * @param team the team the king belongs to.
+	 * 
+	 * @return a valid king
+	 * 
+	 * @throws InvalidParameterException if the team is set as {@link TeamType#NONE}
+	 * 
+	 * @see #Figure(FigureType, FigureType, TeamType)
+	 */
+	public static Figure getKing(TeamType team) {
+		return new Figure(FigureType.KING, FigureType.UNDEFINED, team);
+	}
+
+	/**
+	 * Gets a valid instance of this class representing a tower according to the
+	 * game rules belonging to the team. Has to be used instead of Constructor.
+	 * 
+	 * @param team the team the tower belongs to.
+	 * 
+	 * @return a valid tower
+	 * 
+	 * @throws InvalidParameterException if the team is set as {@link TeamType#NONE}
+	 * 
+	 * @see #Figure(FigureType, FigureType, TeamType)
+	 */
+	public static Figure getTower(TeamType team) {
+		return new Figure(FigureType.TOWER, FigureType.DRAGON, team);
+	}
+
+	/**
+	 * Gets a valid instance of this class representing a bishop according to the
+	 * game rules belonging to the team. Has to be used instead of Constructor.
+	 * 
+	 * @param team the team the bishop belongs to.
+	 * 
+	 * @return a valid bishop
+	 * 
+	 * @throws InvalidParameterException if the team is set as {@link TeamType#NONE}
+	 * 
+	 * @see #Figure(FigureType, FigureType, TeamType)
+	 */
+	public static Figure getBishop(TeamType team) {
+		return new Figure(FigureType.BISHOP, FigureType.HORSE, team);
+	}
+
+	/**
+	 * Gets a valid instance of this class representing a golden general according
+	 * to the game rules belonging to the team. Has to be used instead of
+	 * Constructor.
+	 * 
+	 * @param team the team the golden general belongs to.
+	 * 
+	 * @return a valid golden general
+	 * 
+	 * @throws InvalidParameterException if the team is set as {@link TeamType#NONE}
+	 * 
+	 * @see #Figure(FigureType, FigureType, TeamType)
+	 */
+	public static Figure getGoldenGeneral(TeamType team) {
+		return new Figure(FigureType.GOLDEN_GENERAL, FigureType.UNDEFINED, team);
+	}
+
+	/**
+	 * Gets a valid instance of this class representing a silver general according
+	 * to the game rules belonging to the team. Has to be used instead of
+	 * Constructor.
+	 * 
+	 * @param team the team the silver general belongs to.
+	 * 
+	 * @return a valid silver general
+	 * 
+	 * @throws InvalidParameterException if the team is set as {@link TeamType#NONE}
+	 * 
+	 * @see #Figure(FigureType, FigureType, TeamType)
+	 */
+	public static Figure getSilverGeneral(TeamType team) {
+		return new Figure(FigureType.SILVER_GENERAL, FigureType.GOLDEN_GENERAL, team);
+	}
+
+	/**
+	 * Gets a valid instance of this class representing a knight according to the
+	 * game rules belonging to the team. Has to be used instead of Constructor.
+	 * 
+	 * @param team the team the knight belongs to.
+	 * 
+	 * @return a valid knight
+	 * 
+	 * @throws InvalidParameterException if the team is set as {@link TeamType#NONE}
+	 * 
+	 * @see #Figure(FigureType, FigureType, TeamType)
+	 */
+	public static Figure getKnight(TeamType team) {
+		return new Figure(FigureType.KNIGHT, FigureType.GOLDEN_GENERAL, team);
+	}
+
+	/**
+	 * Gets a valid instance of this class representing a lance according to the
+	 * game rules belonging to the team. Has to be used instead of Constructor.
+	 * 
+	 * @param team the team the lance belongs to.
+	 * 
+	 * @return a valid lance
+	 * 
+	 * @throws InvalidParameterException if the team is set as {@link TeamType#NONE}
+	 * 
+	 * @see #Figure(FigureType, FigureType, TeamType)
+	 */
+	public static Figure getLance(TeamType team) {
+		return new Figure(FigureType.LANCE, FigureType.GOLDEN_GENERAL, team);
+	}
+
+	/**
+	 * Gets a valid instance of this class representing a pawn according to the game
+	 * rules belonging to the team. Has to be used instead of Constructor.
+	 * 
+	 * @param team the team the pawn belongs to.
+	 * 
+	 * @return a valid pawn
+	 * 
+	 * @throws InvalidParameterException if the team is set as {@link TeamType#NONE}
+	 * 
+	 * @see #Figure(FigureType, FigureType, TeamType)
+	 */
+	public static Figure getPawn(TeamType team) {
+		return new Figure(FigureType.PAWN, FigureType.GOLDEN_GENERAL, team);
+	}
+
+	/**
+	 * Gets a valid instance of this class representing a dragon according to the
+	 * game rules belonging to the team. Has to be used instead of Constructor.
+	 * 
+	 * @param team the team the dragon belongs to.
+	 * 
+	 * @return a valid dragon
+	 * 
+	 * @throws InvalidParameterException if the team is set as {@link TeamType#NONE}
+	 * 
+	 * @see #Figure(FigureType, FigureType, TeamType)
+	 */
+	public static Figure getDragon(TeamType team) {
+		return new Figure(FigureType.PAWN, FigureType.UNDEFINED, team);
+	}
+
+	/**
+	 * Gets a valid instance of this class representing a horse according to the
+	 * game rules belonging to the team. Has to be used instead of Constructor.
+	 * 
+	 * @param team the team the horse belongs to.
+	 * 
+	 * @return a valid horse
+	 * 
+	 * @throws InvalidParameterException if the team is set as {@link TeamType#NONE}
+	 * 
+	 * @see #Figure(FigureType, FigureType, TeamType)
+	 */
+	public static Figure getHorse(TeamType team) {
+		return new Figure(FigureType.PAWN, FigureType.UNDEFINED, team);
 	}
 }

0903b3133cb2b1fb8ae2c2b6e4f0f5c0a3d9b3eb
Jonas Etzbach <jonasetz.mc@gmail.com>
2020-07-08 12:19:41 +0200

    implemented check and checkmate temporary commentary patch

--- Shogi/src/beans/GameBean.java
+++ Shogi/src/beans/GameBean.java
@@ -5,14 +5,23 @@ import dataclasses.History;
 import dataclasses.Player;
 import enums.TeamType;
 
+/**
+ * The an instance of this class is a bean representing a game. The name of the
+ * players should be set and if there is a bot in this game the bot should set
+ * right after instantiation.
+ *
+ */
 public class GameBean {
 
 	private int round;
 	private final History history;
-	private final Player white;
-	private final Player black;
+	private Player white;
+	private Player black;
 	private final GameBoard board;
 
+	/**
+	 * The constructor of this class.
+	 */
 	public GameBean() {
 		round = 0;
 		history = new History(10);
@@ -21,23 +30,57 @@ public class GameBean {
 		board = new GameBoard(9);
 	}
 
+	/**
+	 * Getter of the history.
+	 * 
+	 * @return the history
+	 */
 	public History getHistory() {
 		return history;
 	}
 
+	/**
+	 * Getter of the white player.
+	 * 
+	 * @return the white player
+	 */
 	public Player getWhite() {
 		return white;
 	}
 
+	/**
+	 * Getter of the black player.
+	 * 
+	 * @return the black player
+	 */
 	public Player getBlack() {
 		return black;
 	}
 
+	/**
+	 * Getter of the game board.
+	 * 
+	 * @return the game board
+	 */
 	public GameBoard getBoard() {
 		return board;
 	}
 
-	public Player getCurrentPlayer() {
-		return (++round % 2 == 1) ? white : black;
+	/**
+	 * Setter of the white player, should only be used if the white player is an AI
+	 * 
+	 * @param white the ai player
+	 */
+	public void setWhite(Player white) {
+		this.white = white;
+	}
+
+	/**
+	 * Setter of the black player, should only be used if the black player is an AI
+	 * 
+	 * @param black the ai player
+	 */
+	public void setBlack(Player black) {
+		this.black = black;
 	}
 }
